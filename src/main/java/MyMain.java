6767dafe400a31e008a6eba81d7102375143c2d5
Yukai2019 <ysyuki86@gmail.com>
2020-10-13 15:51:31 +0000

    SOlved the error and this code is finally working

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,30 +1,42 @@
-import java.util.Scanner;
-
 public class MyMain {
+    public static int median(final int a, final int b, final int c) {
+        // Checking for b
+        if ((a < b && b < c) || (c < b && b < a))
+            return b;
 
-    // Calculates the median of the three inputs
-    public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        // Checking for a
+        else if ((b < a && a < c) || (c < a && a < b))
+            return a;
+
+        else
+            return c;
+
     }
 
-    // Returns the input with the larger absolute value
-    public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+    public static int magnitude(final int a, final int b) {
+        if (a < b)
+            return b;
+        else
+            return a;
     }
 
-    // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
-    // where "a" and "b" are the inputs to the method
-    public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+    public static double pythagoras(final int a, final int b) {
+        int c = (a+b)^ (1/2);
+        return c;
+
     }
+public static void main(final String[] args) {
+    // You may want to keep these lines of code to test that your methods work
+    System.out.println(median(1, 2, 3)); // should be 2
+    System.out.println(magnitude(-7, -1)); // should be -7
+    System.out.println(pythagoras(3, 4)); // should be 5.0
 
-    public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
-
-        Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+    //final Scanner scan = new Scanner(System.in);
+    //system.out.println("Enter the int");
+    //final Object scanner;
+    //number = scanner.nextInt();
+        
     }
 }
+
+   
